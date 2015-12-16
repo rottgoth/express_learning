4f7da5bf0bfb37195badb7957e5d0b5e29eb5b44
Roberto Cataneo <robcataneo@gmail.com>
2015-12-16 00:37:45 -0600

    Pre-processing user params before requests are handled using app.param

--- 3-user_params/app.js
+++ 3-user_params/app.js
@@ -3,24 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/blocks', function(request, response) {
-  // use of query params
-  var blocks = ['First', 'Second', 'Third'];
-  if (request.query.limit >= 0) {
-    response.json(blocks.slice(0, request.query.limit));
-  } else {
-    response.json(blocks);
-  }
-})
-
 var blocks = {
   'First': 'First element of the list',
   'Second': 'The new element of the list',
   'Third': 'Last element of the list'
 };
 
+app.get('/blocks', function(request, response) {
+  // use of query params
+  var blockKeys = Object.keys(blocks);
+  if (request.query.limit >= 0) {
+    response.json(blockKeys.slice(0, request.query.limit));
+  } else {
+    response.json(blockKeys);
+  }
+})
+
+app.param('name', function (request, response, next) {
+  var name = request.params.name;
+  var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  request.blockName = block;
+  next();
+});
+
 app.get('/blocks/:name', function(request, response) {
-  var description = blocks[request.params.name];
+  var description = blocks[request.blockName];
   if (!description) {
     response.status(404).json('No description found for ' + request.params.name);
   } else {
